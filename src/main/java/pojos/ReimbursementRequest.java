4954510c3754bac28ae489dc841701ed39a25e71
Hannah Novack <hmnov@umich.edu>
2020-11-18 09:52:28 -0500

    adding more pojos

--- src/main/java/pojos/ReimbursementRequest.java
+++ src/main/java/pojos/ReimbursementRequest.java
@@ -1,5 +1,26 @@
 package pojos;
 
 public class ReimbursementRequest {
+	
+	
+	//if the request is turned in less than 2 weeks before the event start date, it is marked as urgent
+	private boolean isUrgent;
+	
+	private boolean isApproved;
 
+	public boolean isUrgent() {
+		return isUrgent;
+	}
+
+	public void setUrgency(boolean isUrgent) {
+		this.isUrgent = isUrgent;
+	}
+
+	public boolean isApproved() {
+		return isApproved;
+	}
+
+	public void approveRequest(boolean isApproved) {
+		this.isApproved = isApproved;
+	}
 }
