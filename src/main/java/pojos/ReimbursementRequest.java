ee14cfcd4f476d8773c64d6a9371145010eb1520
Hannah Novack <hmnov@umich.edu>
2020-11-18 21:38:40 -0500

    more to the pojos!

--- src/main/java/pojos/ReimbursementRequest.java
+++ src/main/java/pojos/ReimbursementRequest.java
@@ -1,12 +1,22 @@
 package pojos;
 
 public class ReimbursementRequest {
+//	This form must collect (required): basic employee information; 
+//	date, time, location, description, cost, grading format,
+//	and type of event; work-related justification.  
 	
+	private Requestor requestor;
+	
+	private double projectedReimbursement;
 	
 	//if the request is turned in less than 2 weeks before the event start date, it is marked as urgent
 	private boolean isUrgent;
 	
-	private boolean isApproved;
+	private boolean isPending;
+	
+	private boolean isApprovedByDirectSupervisor;
+	
+	private boolean isApprovedByBenCo;
 
 	public boolean isUrgent() {
 		return isUrgent;
@@ -16,11 +26,44 @@ public class ReimbursementRequest {
 		this.isUrgent = isUrgent;
 	}
 
-	public boolean isApproved() {
-		return isApproved;
+	public boolean isPending() {
+		return isPending;
 	}
 
-	public void approveRequest(boolean isApproved) {
-		this.isApproved = isApproved;
+	public void setPending(boolean isPending) {
+		this.isPending = isPending;
 	}
+
+	public Requestor getRequestor() {
+		return requestor;
+	}
+
+	public void setRequestor(Requestor requestor) {
+		this.requestor = requestor;
+	}
+
+	public double getProjectedReimbursement() {
+		return projectedReimbursement;
+	}
+
+	public void setProjectedReimbursement(double projectedReimbursement) {
+		this.projectedReimbursement = projectedReimbursement;
+	}
+
+	public boolean isApprovedByDirectSupervisor() {
+		return isApprovedByDirectSupervisor;
+	}
+
+	public void setApprovedByDirectSupervisor(boolean isApprovedByDirectSupervisor) {
+		this.isApprovedByDirectSupervisor = isApprovedByDirectSupervisor;
+	}
+
+	public boolean isApprovedByBenCo() {
+		return isApprovedByBenCo;
+	}
+
+	public void setApprovedByBenCo(boolean isApprovedByBenCo) {
+		this.isApprovedByBenCo = isApprovedByBenCo;
+	}
+
 }
