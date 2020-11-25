11136a983c750ca2ebc03441f85d71dbf5f13d8f
Hannah Novack <hmnov@umich.edu>
2020-11-25 08:34:32 -0500

    event result pojo done

--- src/main/java/pojos/EventResult.java
+++ src/main/java/pojos/EventResult.java
@@ -1,5 +1,45 @@
 package pojos;
 
-public class EventResult {
+import java.io.File;
 
+public class EventResult {
+	
+	private ReimbursementRequest req;
+	
+	private String grade;
+	
+	private File presentation;
+	
+	
+
+	public EventResult(ReimbursementRequest req, String grade, File presentation) {
+		super();
+		this.req = req;
+		this.grade = grade;
+		this.presentation = presentation;
+	}
+
+	public ReimbursementRequest getReq() {
+		return req;
+	}
+
+	public void setReq(ReimbursementRequest req) {
+		this.req = req;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public File getPresentation() {
+		return presentation;
+	}
+
+	public void setPresentation(File presentation) {
+		this.presentation = presentation;
+	}
 }
