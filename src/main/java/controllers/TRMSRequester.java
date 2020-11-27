166dd42bc6a3cd0a9f4a582602dbaeee090d2c94
Hannah Novack <hmnov@umich.edu>
2020-11-27 10:24:31 -0500

    TRMS Requester controller

--- src/main/java/controllers/TRMSRequester.java
+++ src/main/java/controllers/TRMSRequester.java
@@ -1,5 +1,30 @@
 package controllers;
 
+import io.javalin.http.Context;
+
 public class TRMSRequester {
+	
+	//creates request form with appropriate information
+	public void createRequest(Context ctx) {
+		
+	}
+	
+	//if more information is requested
+	public void sendMessage(Context ctx) {
+		
+	}
+	
+	//read request for more information
+	public void viewMessage(Context ctx) {
+		
+	}
+	//for benco approval
+	public void uploadGrade(Context ctx) {
+		
+	}
+	//for direct supervisor approvla
+	public void uploadPresentation(Context ctx) {
+		
+	}
 
 }
